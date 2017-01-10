aaa274bcc74d9f17be0c23bc08a1a612b125de49
samelie <sam@stinkdigital.com>
2017-01-10 15:59:34 -0800

    rad redis

--- routes.js
+++ routes.js
@@ -13,6 +13,10 @@ module.exports = function(github) {
         });
     }
 
+    github.on('rad-redis:refs/heads/master', function(data) {
+        _exec('rad-redis')
+    })
+
     github.on('chewb-live:refs/heads/master', function(data) {
         _exec('chewb-live')
     })
---
+++ sh/rad-redis.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+REPO='git@github.com:jacobjobelie/rad-redis.git';
+RELEASE="/home/samradelie/www/rad/rad-redis/";
+
+# Fetch Latest Code
+pm2 stop rad-redis
+pm2 delete rad-redis
+if [ -d "$RELEASE" ]; then
+  cd $RELEASE
+  git pull origin master
+else
+  git clone -b master $REPO $RELEASE;
+fi
+cd $RELEASE
+npm i
+pm2 start index.js --name rad-redis -f
