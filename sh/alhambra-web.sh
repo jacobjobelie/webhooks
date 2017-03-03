532f7daa19af93a19343255d589c965dc1e60a38
samelie <sam@stinkdigital.com>
2017-03-03 09:36:19 -0800

    syntax

--- sh/alhambra-web.sh
+++ sh/alhambra-web.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REPO='git@github.com:jacojobelie/alhambra-web.git';
+REPO='git@github.com:jacobjobelie/alhambra-web.git';
 RELEASE="/home/samradelie/www/rad/alhambra-web/";
 
 # Fetch Latest Code
